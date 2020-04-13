37c6027a9eee2f48e98f0820ad5e53f576ba1563
vishaalagartha <vishaalagartha@gmail.com>
2020-04-13 14:59:51 -0700

    creates constants and fetches data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as d3 from 'd3';
+import datasets from 'constants/tables.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component { 
+
+  constructor(props){
+    super(props)
+    this.state = {
+      datasets,
+      chosenDataset: datasets[0]
+    }
+  }
+
+  componentDidMount() {
+    d3.csv(this.state.chosenDataset.url)
+      .then(data => {
+          console.log(data)
+        }
+      )
+  }
+
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;
